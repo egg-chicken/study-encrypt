be4ce03a57b25619a8682f859cb7711b6eab807e
eggchicken <no.eggchicken@gmail.com>
2018-04-27 15:32:48 +0900

    Create bin/parameters

--- lib/study/encrypt.rb
+++ lib/study/encrypt.rb
@@ -1,7 +1,19 @@
 require "study/encrypt/version"
+require "ecdsa"
 
 module Study
   module Encrypt
-    # Your code goes here...
+    class << self
+      def print_parameters
+        group = ECDSA::Group::Secp256k1
+        puts "ECDSA::Group::Secp256k1 のパラメータ"
+        puts "素数    ... #{group.field.prime}"
+        puts "生成元x ... #{group.generator.x}"
+        puts "生成元y ... #{group.generator.y}"
+        puts "係数a   ... #{group.param_a}"
+        puts "係数b   ... #{group.param_b}"
+        puts "位数    ... #{group.order}"
+      end
+    end
   end
 end
